15dfa7246cdefcd5614416e79e0205d8c525d517
KozubIgn <ignacykozub@gmail.com>
2020-09-30 14:28:57 +0200

    add add-task component

---
+++ src/app/compoments/add-task/add-task.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  @Output() addTask: EventEmitter<any> = new EventEmitter();
+
+  title: string;
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  addNewTask() {
+    const task = {
+      title: this.title,
+      done: false
+    };
+    this.addTask.emit(task);
+  }
+
+}
